e2993b622afec6119bdbb905fb011faffde9bcb8
namemc95 <moralesdreiser5@gmail.com>
2024-05-02 13:46:34 -0500

    Learning 2 done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,41 +2,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        while (true) 
-        {
-            Console.WriteLine("Menu");
-            Console.WriteLine("Select 1 option in numbers: " );
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
-            Console.WriteLine("5. Quit");
-            Console.Write("What do you want to do? ");
+       
+       Job job1 = new Job ();
+       job1._jobTitle = "Software Engineer";
+       job1._company = "Microsft";
+       job1._startYear = 2019;
+       job1._endYear = 2022;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-            string choice = Console.ReadLine();
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
 
-            switch (choice)
-            {
-                case "1":
-                    Console.WriteLine("Want to write");
-                    break;
-                case "2":
-                    Console.WriteLine("Want to Display");
-                    break;
-                case "3":
-                    Console.WriteLine("Want to Load");
-                    break;
-                case "4":
-                    Console.WriteLine("Want to Save");
-                    break;
-                case "5":
-                    Console.WriteLine("Want to Quit");
-                    break;
-                default:
-                    Console.WriteLine($"This {choice} option is not valid");
-                    break;
-            }
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
 
-        }
+        myResume.Display();
+
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,175 +1,214 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int x = 0;
-        int y = 0;
 
-        //if stament 
-        if (x > y)
+        public class Person 
         {
-            Console.WriteLine("greater than");
-        }
 
+        // The C# convention is to start member variables with an underscore _
+            public string _givenName = "";
+            public string _familyName = "";
 
-        // if inside if 
-        if (x > y)
-        {
-            if (x > z)
+            public Person ()
             {
-                Console.WriteLine("greater than both");
+            }
+
+        // A method that displays the person's full name as used in eastern 
+        // countries or <family name, given name>.
+
+            public void ShowEasternName () 
+            {
+            Console.WriteLine ($"{_familyName}, {_givenName}" );
+            }
+
+        // A method that displays the person's full name as used in western 
+        // countries or <given name family name>.
+            public void ShowWesternName ()
+            {
+                Console.WriteLine ($"{_givenName} {_familyName}" );
             }
         }
 
-
-
-        // if and else
-        if (x > y)
-        {   
-            Console.WriteLine("greater than");
-        }
-        else
+        public class Blind
         {
-            Console.WriteLine("less than");
+            public double _width;
+            public double _height;
+            public string _color;
         }
+    }
+//         int x = 0;
+//         int y = 0;
+
+//         //if stament 
+//         if (x > y)
+//         {
+//             Console.WriteLine("greater than");
+//         }
 
 
-        // else if 
-        if (x > y)
-        {
-            Console.WriteLine("greater than y");
-        }
-        else if (x > z)
-        {
-            Console.WriteLine("greater than z");
-        }
-        else
-        {
-            Console.WriteLine("less than both");
-        }
-
-        //converting type  string to int 
-        string valueInText = "42";
-        int number = int.Parse(valueInText);
-
-        Console.Write("What is your favorite number? ");
-        string userInput = Console.ReadLine();
-        int number = int.Parse(userInput);
-        Console.Write("");  
+//         // if inside if 
+//         if (x > y)
+//         {
+//             if (x > z)
+//             {
+//                 Console.WriteLine("greater than both");
+//             }
+//         }
 
 
-        //int to string 
-        int number = 42;
-        string textVersion = number.ToString();
+
+//         // if and else
+//         if (x > y)
+//         {   
+//             Console.WriteLine("greater than");
+//         }
+//         else
+//         {
+//             Console.WriteLine("less than");
+//         }
+
+
+//         // else if 
+//         if (x > y)
+//         {
+//             Console.WriteLine("greater than y");
+//         }
+//         else if (x > z)
+//         {
+//             Console.WriteLine("greater than z");
+//         }
+//         else
+//         {
+//             Console.WriteLine("less than both");
+//         }
+
+//         //converting type  string to int 
+//         string valueInText = "42";
+//         int number = int.Parse(valueInText);
+
+//         Console.Write("What is your favorite number? ");
+//         string userInput = Console.ReadLine();
+//         int number = int.Parse(userInput);
+//         Console.Write("");  
+
+
+//         //int to string 
+//         int number = 42;
+//         string textVersion = number.ToString();
+// /////////////////////////////////////////////////////////////////////////
+
+//         // do whille 
+//         string response;
+
+//         do
+//         {
+//             Console.Write("Do you want to continue? ");
+//             response = Console.ReadLine();
+//         } while (response == "yes");
+
+//         // foreach loop
+//         foreach (string color in colors)
+//         {
+//             Console.WriteLine(color);
+//         }
+
+//         // randon generator 
+//         Random randomGenerator = new Random();
+//         int number = randomGenerator.Next(1, 11);
+
+// /////////////////////////////////////////////////////////////////////////
+// ///
+//     // lists 
+//     List<int> numbers;
+//     List<string> words;
+
+
+//     List<int> numbers;
+//     numbers = new List<int>();
+
+//     List<int> numbers = new List<int>();
+//     List<string> words = new List<string>();
+
+//     // adding items 
+
+//     List<string> words = new List<string>();
+
+//     words.Add("phone");
+//     words.Add("keyboard");
+//     words.Add("mouse");
+
+//     // having the length 
+//     Console.WriteLine(words.Count);
+
+//     //iteratig 
+
+//     foreach (string word in words)
+//     {
+//         Console.WriteLine(word);
+//     }
+
+//     // accesing by index 
+//     for (int i = 0; i < words.Count; i++)
+//     {
+//         Console.WriteLine(words[i]);
+//     }
+
+
+//     //////////////////////////////////////////////////
+//     /// functions 
+//     /// 
+
+//     returnType FunctionName(dataType parameter1, dataType parameter2)
+//     {
+//         // function_body
+//     }
+
+//     // function without parameter 
+//     void DisplayMessage()
+//     {
+//         Console.WriteLine("Hello world!");
+//     }
+
+//     // single parameter 
+//     void DisplayPersonalMessage(string userName)
+//     {
+//         Console.WriteLine($"Hello {userName}");
+//     }
+
+
+//     // 2 int parameters 
+//     int AddNumbers(int first, int second)
+//     {
+//         int sum = first + second;
+//         return sum;
+//     }
+
+//     ///
+//     static void DisplayMessage()
+//     {
+//         Console.WriteLine("Hello world!");
+//     }
+
+//     static void DisplayPersonalMessage(string userName)
+//     {
+//         Console.WriteLine($"Hello {userName}");
+//     }
+
+//     static int AddNumbers(int first, int second)
+//     {
+//         int sum = first + second;
+//         return sum;
+//     }
 /////////////////////////////////////////////////////////////////////////
-
-        // do whille 
-        string response;
-
-        do
-        {
-            Console.Write("Do you want to continue? ");
-            response = Console.ReadLine();
-        } while (response == "yes");
-
-        // foreach loop
-        foreach (string color in colors)
-        {
-            Console.WriteLine(color);
-        }
-
-        // randon generator 
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
-
-/////////////////////////////////////////////////////////////////////////
+///classes 
 ///
-    // lists 
-    List<int> numbers;
-    List<string> words;
+    // A code template for the category of things known as Person. The
+    // responsibility of a Person is to hold and display personal information.
 
 
-    List<int> numbers;
-    numbers = new List<int>();
-
-    List<int> numbers = new List<int>();
-    List<string> words = new List<string>();
-
-    // adding items 
-
-    List<string> words = new List<string>();
-
-    words.Add("phone");
-    words.Add("keyboard");
-    words.Add("mouse");
-
-    // having the length 
-    Console.WriteLine(words.Count);
-
-    //iteratig 
-
-    foreach (string word in words)
-    {
-        Console.WriteLine(word);
-    }
-
-    // accesing by index 
-    for (int i = 0; i < words.Count; i++)
-    {
-        Console.WriteLine(words[i]);
-    }
-
-
-    //////////////////////////////////////////////////
-    /// functions 
-    /// 
-
-    returnType FunctionName(dataType parameter1, dataType parameter2)
-    {
-        // function_body
-    }
-
-    // function without parameter 
-    void DisplayMessage()
-    {
-        Console.WriteLine("Hello world!");
-    }
-
-    // single parameter 
-    void DisplayPersonalMessage(string userName)
-    {
-        Console.WriteLine($"Hello {userName}");
-    }
-
-
-    // 2 int parameters 
-    int AddNumbers(int first, int second)
-    {
-        int sum = first + second;
-        return sum;
-    }
-
-    ///
-    static void DisplayMessage()
-    {
-        Console.WriteLine("Hello world!");
-    }
-
-    static void DisplayPersonalMessage(string userName)
-    {
-        Console.WriteLine($"Hello {userName}");
-    }
-
-    static int AddNumbers(int first, int second)
-    {
-        int sum = first + second;
-        return sum;
-    }
-
-
-    
-
-    }
+  
 }
